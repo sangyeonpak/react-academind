0973c2f601d522f521660feccec20ba321992460
Sangyeon Pak <sangyeonpak1994@gmail.com>
2023-01-14 22:56:29 -0500

    pushing all the progress so far; the tutorial wants me to deploy it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import Todo from './components/Todo.js';
+// import Todo from "./components/Todo.js";
+// function App() {
+//   return (
+//     <div>
+//       <h1>My Todos</h1>
+//       <Todo text="Learn React"/>
+//       <Todo text="Master React"/>
+//       <Todo text="WTF React"/>
+//     </div>
+//   );
+// }
 
+import { Routes, Route } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups.js';
+import NewMeetupPage from './pages/NewMeetup.js';
+import FavoritesPage from './pages/Favorites.js';
+import Layout from './components/layout/Layout.js'
 
 function App() {
   return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React"/>
-      <Todo text="Master React"/>
-      <Todo text="WTF React"/>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<AllMeetupsPage />}/>
+        <Route path='/new-meetup' element={<NewMeetupPage />}/>
+        <Route path='/favorites' element={<FavoritesPage />}/>
+      </Routes>
+    </Layout>
   );
 }
 
--- src/pages/NewMeetups.js
+++
@@ -1,5 +0,0 @@
-function NewMeetupsPage() {
-  return <div>New Meetups Page</div>
-}
-
-export default NewMeetupsPage;
