a097189e4ed5062a6418ea2fa785c3e8ed7fc923
Sangyeon Pak <sangyeonpak1994@gmail.com>
2023-01-15 01:21:58 -0500

    Complete; favorites data doesn't persist.

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,12 +1,28 @@
-import NewMeetupForm from "../components/meetups/NewMeetupForm.js"
+import { useNavigate } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm.js";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+  const addMeetupHandler = (meetupData) => {
+    fetch(
+      "https://react-academind-6779a-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      navigate('/', {replace: true})
+    });
+  };
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
-  )
+  );
 }
 
 export default NewMeetupPage;
